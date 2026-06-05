47e2af0ce0c8fca5c3784e93664fcd1335fbf501
ajay1989 <vyas.ajay10@gmail.com>
2026-06-05 18:48:07 +0530

    Initial Xcode project setup

---
+++ RemoteRecruitTests/RemoteRecruitTests.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteRecruitTests.swift
+//  RemoteRecruitTests
+//
+//  Created by Ajay Vyas on 05/06/26.
+//
+
+import Testing
+@testable import RemoteRecruit
+
+struct RemoteRecruitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
